a65b807c2b8618e6bc558e6b6e5edc875cbce473
David <sotosanchezdavid0@gmail.com>
2025-03-19 19:09:02 +0100

    Primer commit en Main

---
+++ Ds_Circulo.java
@@ -0,0 +1,36 @@
+public class Ds_Circulo {
+    private int id;
+    private double radio;
+    private String color;
+    public Ds_Circulo() {
+        this.radio = 0.0;
+        this.color = "Desconocido";
+    }
+    public Ds_Circulo(double radio, String color) {
+        this.radio = radio;
+        this.color = color;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public int getId() {
+        return id;
+    }
+    public double getRadio() {
+        return radio;
+    }
+    public String getColor() {
+        return color;
+    }
+    @Override
+    public String toString() {
+        return "Circulo { radio=" + radio + ", color='" + color + "' }";
+    }
+}
